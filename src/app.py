0c9b64a35a89c2ddfb277dae9d4acc35ac46606d
Shafiullah Noorbasha <shafiknoor@gmail.com>
2020-10-25 15:49:51 +0530

    Add files via upload
    Initial Commit

---
+++ src/app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 11:54:15 2020
+
+@author: AF65547
+This is the entry point for python recommendation engine.
+"""
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+class HelloWorld1(Resource):
+    def ping(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld1, '/ping')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
